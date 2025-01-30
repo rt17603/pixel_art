8717870c454de129bcf600b99bdddb052ffbb78a
Rachel Tunnicliffe <rachel.tunnicliffe@bristol.ac.uk>
2025-01-30 12:08:18 +0000

    Updating bee's wing colour to be lighter

--- pixel_art.py
+++ pixel_art.py
@@ -7,7 +7,7 @@ def define_bee():
     # define some colours
     black = [0, 0, 0]
     yellow = [1.0, 0.85, 0]
-    grey = [0.65] * 3
+    grey = [0.9] * 3
 
     # specify which pixels are which colour
     image_mat[7:11, 2] = black
