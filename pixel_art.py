958f0f3885a76c4b6a6b32f3f76776ec54b19f28
Hodan-1 <hodanabdi24@outlook.com>
2025-01-30 11:31:25 +0000

    Changed bee wing colours

--- pixel_art.py
+++ pixel_art.py
@@ -1,3 +1,5 @@
+##testing forking
+
 def define_bee():
     import numpy as np
 
@@ -10,11 +12,11 @@ def define_bee():
     grey = [0.65] * 3
 
     # specify which pixels are which colour
-    image_mat[7:11, 2] = black
-    image_mat[6:12, 3:5] = black
+    image_mat[7:11, 2] = yellow
+    image_mat[6:12, 3:5] = grey
     image_mat[6:12, 5:7] = yellow
-    image_mat[6:12, 7:9] = black
-    image_mat[6:12, 9:11] = yellow
+    image_mat[6:12, 7:9] = yellow
+    image_mat[6:12, 9:11] = grey
     image_mat[6:12, 11:13] = black
     image_mat[7:11, 13] = black
     image_mat[4:6, 5:11] = grey
