4b7911a962ba86cd6e6c006bc28a91170fda3a14
Rachel Tunnicliffe <rachel.tunnicliffe@bristol.ac.uk>
2025-01-30 11:44:12 +0000

    Adding butterfly picture

--- pixel_art.py
+++ pixel_art.py
@@ -22,6 +22,54 @@ def define_bee():
     
     return image_mat
 
+def define_butterfly():
+    import numpy as np
+
+    # make an empty numpy array for storing the image
+    image_mat = np.full((16, 16, 3), 1.0)
+
+    # define some colours
+    black = [0, 0, 0]
+    blue = [0.2, 0.4, 0.8]
+
+    # specify which pixels are which colour
+    c = 8
+    # centre of butterfly
+    image_mat[2:13, c] = black
+    
+    # generate mirror image at the same time
+    for i in [-1, 1]:
+        # outline
+        image_mat[1, i*1+c] = black
+        image_mat[0, i*2+c] = black
+        image_mat[4, i*1+c] = black
+        image_mat[3, [i*j+c for j in [2, 3]]] = black
+        image_mat[2, [i*j+c for j in [4, 5]]] = black
+        image_mat[3, i*6+c] = black
+        image_mat[4:7, i*7+c] = black
+        image_mat[7, [i*j+c for j in [6, 5]]] = black
+        image_mat[8, [i*j+c for j in [4, 3, 2]]] = black
+        image_mat[7, i*1+c] = black
+        image_mat[9, i*5+c] = black
+        image_mat[10:12, i*6+c] = black
+        image_mat[12, i*5+c] = black
+        image_mat[13, [i*j+c for j in [4, 3]]] = black
+        image_mat[12, i*2+c] = black
+        image_mat[11, i*1+c] = black
+        
+        # fill in the centre line by line
+        image_mat[3, [i*j+c for j in [4, 5]]] = blue
+        image_mat[4, [i*j+c for j in range(2,7)]] = blue
+        image_mat[5:7, [i*j+c for j in range(1,7)]] = blue
+        image_mat[7, [i*j+c for j in range(2,5)]] = blue
+        image_mat[8, i*1+c] = blue
+        image_mat[9, [i*j+c for j in range(1,5)]] = blue
+        image_mat[10, [i*j+c for j in range(1,6)]] = blue
+        image_mat[11, [i*j+c for j in range(2,6)]] = blue
+        image_mat[12, [i*j+c for j in range(3,5)]] = blue
+    
+    return image_mat
+
 
 def plot_image(image):
     import matplotlib.pyplot as plt
@@ -33,3 +81,5 @@ def plot_image(image):
     
 bee = define_bee()
 plot_image(bee)
+butterfly = define_butterfly()
+plot_image(butterfly)
